fc84b4a776ecba69bf85c384ce680b8e61b2b29b
serebrennikova <58689329+serebrennikova@users.noreply.github.com>
2020-12-05 12:22:26 +0500

    Update server3000.js

--- server3000.js
+++ server3000.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var server = new http.Server();
 server.listen(3000, '127.0.0.1');
 server.on('request', function(req, res){
-res.setHeader('content-type', 'text/html');.
+res.setHeader('content-type', 'text/html');
 fs.readFile("index.html", {encoding: 'utf-8'}, function(err, data){
  if(err){
  console.error(err.message);
